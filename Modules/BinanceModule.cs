0f4692ebb8ad6a3926bf9ba384f14a45a61f05fb
Wesley Taylor <wesley.taylor303@gmail.com>
2017-12-21 13:17:37 -0500

    New object to find BTC - USDT value.

--- Modules/BinanceModule.cs
+++ Modules/BinanceModule.cs
@@ -27,32 +27,43 @@ namespace Bittrex_Bot.Modules
             //default input parameter to be paired with BTC
             _coin = _coin + "BTC";
             Console.WriteLine(_coin);
-            var apiStartingValue = $"https://www.binance.com/api/v1/ticker/24hr?symbol={_coin}";
+            var apiStartingAddress = $"https://www.binance.com/api/v1/ticker/24hr?symbol={_coin}";
+            var bitcoinDollarValue = $"https://www.binance.com/api/v1/ticker/24hr?symbol=BTCUSDT";
 
             //open web client
             var client = new WebClient();
             //download the json into a string
-            string jsonRaw = client.DownloadString(apiStartingValue);
+            string jsonRaw = client.DownloadString(apiStartingAddress);
+            //download BTC value
+            string BTCUSDT = client.DownloadString(bitcoinDollarValue);
             //parse the json into a raw object
             dynamic binanceObj = JObject.Parse(jsonRaw);
+            //another json object to computer dollar values
+            dynamic bitcoinObj = JObject.Parse(BTCUSDT);
+
+            //Value to computer XXX coin to USDT values
+            //real goal here needs to use USD but....
+            double USDTprice = bitcoinObj["lastPrice"];
 
             //declare the variables that need to be output
             string symbol = binanceObj["symbol"];
-            string lastPrice = binanceObj["lastPrice"];
+            double lastPrice = binanceObj["lastPrice"];
             string twentyfourHourChange = binanceObj["priceChange"];
+            double lastPriceDollaredOut = (USDTprice * lastPrice);
 
           
 
 
             await ReplyAsync(
                 "```" +
-                "Symbol" + symbol + "\n" +
 
-                "Last Price" + lastPrice + "\n" +
+                "Symbol: " + symbol + "\n" +
 
-                "24 Hour Change" + twentyfourHourChange + "\n" 
+                "Last Price: " + lastPrice + "\n" +
 
-                //"Symbol" + +
+                "24 Hour Change: " + twentyfourHourChange + "\n" +
+
+                "Last Price USDT:" + lastPriceDollaredOut + "\n" 
 
                 //"Symbol" + +
 
