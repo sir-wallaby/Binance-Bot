a580beee67be54b74f5f0255707df784e8a57508
Wesley Taylor <wesley.taylor303@gmail.com>
2017-12-21 13:52:20 -0500

    Added boolean to sort of error check if BTC was entered so it wouldn't append BTC and would multiply BTC * BTC

--- Modules/BinanceModule.cs
+++ Modules/BinanceModule.cs
@@ -24,53 +24,81 @@ namespace Bittrex_Bot.Modules
         public async Task BinanceTicker([Remainder] string coin)
         {
             string _coin = coin.ToUpper();
-            //default input parameter to be paired with BTC
-            _coin = _coin + "BTC";
-            Console.WriteLine(_coin);
-            var apiStartingAddress = $"https://www.binance.com/api/v1/ticker/24hr?symbol={_coin}";
-            var bitcoinDollarValue = $"https://www.binance.com/api/v1/ticker/24hr?symbol=BTCUSDT";
+            Boolean wasBitcoinEntered = false;
 
-            //open web client
-            var client = new WebClient();
-            //download the json into a string
-            string jsonRaw = client.DownloadString(apiStartingAddress);
-            //download BTC value
-            string BTCUSDT = client.DownloadString(bitcoinDollarValue);
-            //parse the json into a raw object
-            dynamic binanceObj = JObject.Parse(jsonRaw);
-            //another json object to computer dollar values
-            dynamic bitcoinObj = JObject.Parse(BTCUSDT);
+            try
+            {
+                //default input parameter to be paired with BTC
+                _coin = _coin + "BTC";
+                //if block when searching for BTC just default to BTCUSDT
+                if (_coin == "BTCBTC")
+                {
+                    _coin = "BTCUSDT";
+                    wasBitcoinEntered = true;
 
-            //Value to computer XXX coin to USDT values
-            //real goal here needs to use USD but....
-            double USDTprice = bitcoinObj["lastPrice"];
+                }
 
-            //declare the variables that need to be output
-            string symbol = binanceObj["symbol"];
-            double lastPrice = binanceObj["lastPrice"];
-            string twentyfourHourChange = binanceObj["priceChange"];
-            double lastPriceDollaredOut = (USDTprice * lastPrice);
+                //Console.WriteLine(_coin);
+                var apiStartingAddress = $"https://www.binance.com/api/v1/ticker/24hr?symbol={_coin}";
+                var bitcoinDollarValue = $"https://www.binance.com/api/v1/ticker/24hr?symbol=BTCUSDT";
 
+                //open web client
+                var client = new WebClient();
+                //download the json into a string
+                string jsonRaw = client.DownloadString(apiStartingAddress);
+                //download BTC value
+                string BTCUSDT = client.DownloadString(bitcoinDollarValue);
+                //parse the json into a raw object
+                dynamic binanceObj = JObject.Parse(jsonRaw);
+                //another json object to computer dollar values
+                dynamic bitcoinObj = JObject.Parse(BTCUSDT);
+
+                //Value to computer XXX coin to USDT values
+                //real goal here needs to use USD but....
+                double USDTprice = bitcoinObj["lastPrice"];
+
+                //declare the variables that need to be output
+                string symbol = binanceObj["symbol"];
+                double lastPrice = binanceObj["lastPrice"];
+                double twentyfourHourChange = binanceObj["priceChangePercent"];
+
+                double lastPriceDollaredOut;
+                if (wasBitcoinEntered == false)
+                {
+                     lastPriceDollaredOut = (USDTprice * lastPrice);
+
+                }
+                else if (wasBitcoinEntered == true)
+                {
+                    lastPriceDollaredOut = (USDTprice);
+                }
+                else
+                {
+                    lastPriceDollaredOut = 0;
+                }
+
+
+
+                await ReplyAsync(
+                    "```" +
+
+                    "Symbol: " + symbol + "\n" +
+
+                    "24 Hour Change: " + Math.Round(twentyfourHourChange, 2) + "%" + "\n" +
+
+                    "Last Price USDT: $" + Math.Round(lastPriceDollaredOut, 2) + "\n"
+
+                    //"Symbol" + +
+
+                    //"Symbol" + +
+
+                    + "```");
+            }
+            catch (Exception e)
+            {
+                Console.WriteLine("Error: '{0}'", e);
+            }
           
-
-
-            await ReplyAsync(
-                "```" +
-
-                "Symbol: " + symbol + "\n" +
-
-                "Last Price: " + lastPrice + "\n" +
-
-                "24 Hour Change: " + twentyfourHourChange + "\n" +
-
-                "Last Price USDT:" + lastPriceDollaredOut + "\n" 
-
-                //"Symbol" + +
-
-                //"Symbol" + +
-
-                +"```");
-
 
 
 
