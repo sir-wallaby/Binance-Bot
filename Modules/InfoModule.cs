df3a68d691f40e96c1c6ad5f33c6b1785cdb24f3
Wes Taylor <wesley.taylor303@gmail.com>
2018-01-22 00:01:17 -0500

    added info Module and Jail Module.

---
+++ Modules/InfoModule.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Diagnostics;
+using System.Linq;
+using System.Runtime.InteropServices;
+using System.Threading.Tasks;
+using Discord;
+using Discord.Commands;
+
+namespace Binance_Bot.Modules
+{
+    public class InfoModule : ModuleBase<SocketCommandContext>
+    {
+        [Command("info")]
+        [Alias("about", "whoami", "owner")]
+        public async Task InfoAsync()
+        {
+            var app = await Context.Client.GetApplicationInfoAsync();
+
+            await ReplyAsync(
+                $"Binance Bot is a private-use Discord bot for the New Wave server. It's primary use is to pull prices from the Binance public side API.\n\n" +
+                $"{Format.Bold("Info")}\n" +
+                $"- Author: {app.Owner} ({app.Owner.Id})\n" +
+                $"- Library: Discord.Net ({DiscordConfig.Version})\n" +
+                $"- Runtime: {RuntimeInformation.FrameworkDescription} {RuntimeInformation.ProcessArchitecture} " +
+                    $"({RuntimeInformation.OSDescription} {RuntimeInformation.OSArchitecture})\n" +
+                $"- Uptime: {GetUptime()}\n\n" +
+
+                $"{Format.Bold("Stats")}\n" +
+                $"- Heap Size: {GetHeapSize()}MiB\n" +
+                $"- Guilds: {Context.Client.Guilds.Count}\n" +
+                $"- Channels: {Context.Client.Guilds.Sum(g => g.Channels.Count)}\n" +
+                $"- Users: {Context.Client.Guilds.Sum(g => g.Users.Count)}\n");
+        }
+
+        private static string GetUptime() => (DateTime.Now - Process.GetCurrentProcess().StartTime).ToString(@"dd\.hh\:mm\:ss");
+        private static string GetHeapSize() => Math.Round(GC.GetTotalMemory(true) / (1024.0 * 1024.0), 2).ToString();
+    }
+}
---
+++ Modules/Jail.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Discord;
+using Discord.Commands;
+
+
+namespace Binance_Bot.Modules
+{
+    public class Jail : ModuleBase<SocketCommandContext> 
+    {
+        [Name("Send To Jail")]
+        [Command("mute")]
+            public async Task sucker (IGuildUser user)
+        {
+            var role = Context.Guild.Roles.FirstOrDefault(x => x.Name.ToString() == "SuckerRole");
+
+            await (user as IGuildUser).AddRoleAsync(role);
+        }
+
+        [Name("Remove From Jail")]
+        [Command("unmute")]
+        public async Task unSucker(IGuildUser user)
+        {
+            var role = Context.Guild.Roles.FirstOrDefault(x => x.Name.ToString() == "SuckerRole");
+
+            await (user as IGuildUser).RemoveRoleAsync(role);
+        }
+    }
+}
